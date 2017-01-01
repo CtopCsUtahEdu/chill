b1572256298e0e106d2770653bd5c0d49a243e7a
Tuowen Zhao <ztuowen@gmail.com>
2017-01-01 13:04:37 -0700

    added checksum for google-test download

--- cmake/GTest.cmake
+++ cmake/GTest.cmake
@@ -3,6 +3,7 @@ add_custom_target(UnitTests)
 include(ExternalProject)
 ExternalProject_Add(googletest
     URL https://github.com/google/googletest/archive/release-1.8.0.zip
+    URL_MD5 "adfafc8512ab65fd3cf7955ef0100ff5"
     CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/gtest;-DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}"
     LOG_CONFIGURE 1
     LOG_INSTALL 1
