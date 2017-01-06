e1f598a3c8986bf13c370ce34e2ee30e93b58fcf
Tuowen Zhao <ztuowen@gmail.com>
2017-01-06 12:20:41 -0700

    [BUGFIX] Making debug print behave more like a single statement
    All block or other construct in debug print are wrapped inside a single
    do-while block, so that it become atomic and will mandatorily require a
    semicolon.

--- chill_io.hh
+++ chill_io.hh
@@ -7,9 +7,15 @@
 // Stream output (for debugging, and stdout) //
 // ----------------------------------------- //
 
+#ifndef NDEBUG
+#ifndef DEBUGCHILL
+#define DEBUGCHILL
+#endif
+#endif
+
 // Normal chill output
-#define chill_fprintf(f, ...)               { fprintf(f, __VA_ARGS__); fflush(f); }
-#define chill_printf(...)                   { printf(__VA_ARGS__); fflush(stdout); }
+#define chill_fprintf(f, ...)               do { fprintf(f, __VA_ARGS__); fflush(f); } while (0)
+#define chill_printf(...)                   do { printf(__VA_ARGS__); fflush(stdout); } while (0)
 
 // User error messages
 #define chill_error_fprintf(f, ...)         chill_fprintf(f, __VA_ARGS__)
@@ -22,17 +28,17 @@ void debug_define(char* symbol);
 bool debug_isenabled();
 bool debug_isdefined(char* symbol);
 
-#define debug_fprintf(f, ...)               if(debug_isenabled()) { chill_fprintf(f, __VA_ARGS__); }
-#define debug_printf(...)                   if(debug_isenabled()) { chill_printf(__VA_ARGS__); }
-#define debug_cond_fprintf(s, f, ...)       if(debug_isdefined(s)) { debug_fprintf(f, __VA_ARGS__); }
-#define debug_cond_printf(s, ...)           if(debug_isdefined(s)) { debug_printf(__VA_ARGS__); }
+#define debug_fprintf(f, ...)               do { if(debug_isenabled()) { chill_fprintf(f, __VA_ARGS__); } } while(0)
+#define debug_printf(...)                   do { if(debug_isenabled()) { chill_printf(__VA_ARGS__); } } while (0)
+#define debug_cond_fprintf(s, f, ...)       do { if(debug_isdefined(s)) { debug_fprintf(f, __VA_ARGS__); } } while (0)
+#define debug_cond_printf(s, ...)           do { if(debug_isdefined(s)) { debug_printf(__VA_ARGS__); } } while (0)
 
 #else
 
-#define debug_enable(...)
-#define debug_define(...)
-#define debug_fprintf(...)
-#define debug_printf(...)
+#define debug_enable(...)                   do {} while (0)
+#define debug_define(...)                   do {} while (0)
+#define debug_fprintf(...)                  do {} while (0)
+#define debug_printf(...)                   do {} while (0)
 
 #endif
 
