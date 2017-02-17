b83e35beb183786dbfe60fdaeaa219d670acf948
Derick Huth <derickhuth@gmail.com>
2017-02-17 12:01:45 -0700

    more test scritps/source

---
+++ examples/chill/coalesce.script.py
@@ -0,0 +1,14 @@
+from chill import *
+
+source('coalesce.c')
+procedure('main')
+destination('coalescemodified.c')
+loop(0)
+
+
+
+original()
+coalesce(0, coalesced_index, [1,2], c)
+#coalesce_by_index(0,"coalesced_index",{"i","j"}, "c")
+
+#print
---
+++ examples/chill/inc.script.py
@@ -0,0 +1,12 @@
+from chill import *
+
+source('inc.c')
+procedure('main')
+destination('incmodified.c')
+loop(0)
+
+
+original()
+print_code()
+
+
